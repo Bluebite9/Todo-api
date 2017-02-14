b7a0656520a4c5792763bc52834f26eeb7b63071
Adrian N <adrian.c.nedelcu@gmail.com>
2017-02-14 15:30:27 +0200

    Addind todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,43 +19,38 @@ var Todo = sequelize.define('todo', {
     }
 });
 
+var User = sequelize.define('user', {
+    email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({//force: true
 }).then(function() {
     console.log('Everything is synced');
     
-        Todo.findById(1).then(function (todo){
-            if(todo) {
-                console.log(todo.toJSON())
-            } else {
-                console.log('Not found')  
+    User.findById(1).then(function (user) {
+        user.getTodos({
+            where: {
+                completed: true
             }
+        }).then(function (todos) {
+            todos.forEach(function (todo) {
+                console.log(todo.toJSON());
+            });
         });
-    
-//    Todo.create({
-//        description: 'Take out trash',
-//        completed: false
-//    }).then(function (todo) {
+    });
+   
+//    User.create({
+//        email: 'adrian@example.com'
+//    }).then (function () {
 //        return Todo.create({
-//            description: 'Clean office'
+//            description: 'Clean yard'
+//        })
+//    }).then(function (todo) {
+//        User.findById(1).then(function (user) {
+//            user.addTodo(todo);
 //        });
-//    }).then(function () {
-//        //return Todo.findById(1)
-//        return Todo.findAll({
-//           where: {
-//               description: {
-//                    $like: '%Office%'
-//               }
-//           } 
-//        });
-//    }).then(function (todos) {
-//        if (todos) {
-//            todos.forEach(function (todo) {
-//                console.log(todo.toJSON());
-//            });
-//        } else {
-//            console.log('no todo found!');
-//        }
-//    }).catch(function (e){
-//        console.log(e);
 //    });
 });
